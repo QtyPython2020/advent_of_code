8ad0cdde384d4b3cd7a4f265cceef4cf56d1ff9f
QtyPython2020 <htm131016@gmail.com>
2023-06-08 09:54:23 +0200

    update helper functions file and made a project

--- helpers/helper_functions.py
+++ helpers/helper_functions.py
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+@author: QtyPython2020
+
+"""
+
 def read_single_line_to_str(path):
     with open(path, "r") as file:
         content = file.readline()
     return content
 
 if __name__ == "__main__":
-    out = read_single_line_to_str("../2015/input_2015_1.txt")
+    pass
