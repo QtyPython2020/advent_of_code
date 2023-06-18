d2b93f0b4d708a9c0b5db67970ffb4a73fa8f33b
QtyPython2020 <htm131016@gmail.com>
2023-06-18 20:38:18 +0200

    Added new helper function and refactored file

--- helpers/helper_functions.py
+++ helpers/helper_functions.py
@@ -7,22 +7,37 @@ import json
 
 def read_single_line_to_str(path: str) -> str:
     """
-
+    Read the input from the text file at the provided filepath. The file should
+    contain one line and the output will be a string.
     """
-    with open(path, "r") as file:
+    with open(path, mode="r", encoding="utf-8") as file:
         puzzle_input = file.readline().rstrip("\n")
     return puzzle_input
 
-def read_single_line_to_json(path: str):
-    with open(path, "r") as file:
+def read_single_line_to_tuple_of_str(path: str) -> tuple[str]:
+    """
+    Read the input from the text file at the provided filepath. The file should
+    contain one line and the output will be a tuple of strings.
+    """
+    with open(path, mode="r", encoding="utf-8") as file:
+        puzzle_input = tuple(file.readline().rstrip("\n").split(", "))
+    return puzzle_input
+
+def read_json_to_object(path: str) -> (list,dict):
+    """
+    Read the input from the text file at the provided filepath. The file should
+    contain one json object and the output will be a dictionary or list.
+    """
+    with open(path, mode="r", encoding="utf-8") as file:
         puzzle_input = json.loads(file.read())
     return puzzle_input
 
 def read_multiple_lines_to_tuple_of_str(path: str) -> tuple[str]:
     """
-
+    Read the input from the text file at the provided filepath. The file should
+    contain multiple lines and the output will be a tuple of strings.
     """
-    with open(path, "r") as file:
+    with open(path, mode="r", encoding="utf-8") as file:
         puzzle_input = tuple(line.rstrip("\n") for line in file.readlines())
     return puzzle_input
 
