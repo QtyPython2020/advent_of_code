cd7764366f94cf6929a3785527360f1887917c97
QtyPython2020 <htm131016@gmail.com>
2023-06-21 07:44:33 +0200

    Added helper functions and refactored some

--- helpers/helper_functions.py
+++ helpers/helper_functions.py
@@ -13,6 +13,12 @@ def _parse_to_int(element: str) -> tuple[int]:
     """
     return tuple(int(el) for el in re.findall(r"(\d{1,})", element))
 
+def _parse_to_str(element: str, sep: str = None) -> tuple[str]:
+    """
+    Extract all seperate elements from string and convert these to strings
+    """
+    return tuple(element.split(sep=sep))
+
 def read_single_line_to_str(path: str) -> str:
     """
     Read the input from the text file at the provided filepath. The file should
@@ -29,6 +35,11 @@ def read_single_line_to_tuple_of_str(path: str) -> tuple[str]:
     """
     with open(path, mode="r", encoding="utf-8") as file:
         puzzle_input = tuple(file.readline().rstrip("\n").split(", "))
+    return puzzle_input
+
+def read_single_line_to_dict_of_int(path: str) -> dict[int]:
+    with open(path, mode="r", encoding="utf-8") as file:
+        puzzle_input = {index: int(el) for index, el in enumerate(file.readline().rstrip("\n").split())}
     return puzzle_input
 
 def read_json_to_object(path: str) -> Union[list, dict]:
@@ -62,7 +73,16 @@ def read_multiple_lines_to_tuple_of_parsed_element(path: str,
         puzzle_input = tuple(func(line.rstrip("\n")) for line in file.readlines())
     return puzzle_input
 
-PARSERS = {"int": _parse_to_int}
+def read_multiple_lines_to_list_of_ints(path: str):
+    """
+
+    """
+    with open(path, mode="r", encoding="utf-8") as file:
+        puzzle_input = [int(line.rstrip("\n")) for line in file.readlines()]
+    return puzzle_input
+
+PARSERS = {"int": _parse_to_int,
+           "str": _parse_to_str}
 
 if __name__ == "__main__":
     pass
