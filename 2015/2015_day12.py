f689def74f37696bafd4b53fcfe21841a3cedf9f
QtyPython2020 <htm131016@gmail.com>
2023-08-13 21:36:57 +0200

    Solution for 2015 Day 12

--- 2015/2015_day12.py
+++ 2015/2015_day12.py
@@ -25,6 +25,17 @@ def _count_non_red_numbers(json_part, count=0):
     """
 
     """
+    if isinstance(json_part, dict):
+        if "red" in json_part.values():
+            return count
+        json_part = json_part.values()
+    for element in json_part:
+        if isinstance(element, (dict, list)):
+            count = _count_non_red_numbers(element, count)
+        elif isinstance(element, str):
+            count += 0
+        else:
+            count += element
     return count
 
 def part1(commands: str):
@@ -36,4 +47,4 @@ def part2(commands: str):
 if __name__ == "__main__":
     instructions = read_json_to_object("input_2015_12.txt")
     print(part1(instructions))
-    print(part2(instructions[2]))
+    print(part2(instructions))
